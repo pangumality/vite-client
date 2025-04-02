1352d2967a9f891fa7e3966fc6b278fb8b95744e
oneous mality <pangumality@gmail.com>
2025-04-02 04:25:13 +0200

    changed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    },
     proxy: {
       '/api': 'http://localhost:5000',
     },
